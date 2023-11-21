46e9e21be00ea2b21a16c0c80dbbe780dbfe3cec
Try <127165831+trywxyz@users.noreply.github.com>
2023-11-21 00:30:35 -0300

    Add files via upload

---
+++ js/badges.js
@@ -0,0 +1,33 @@
+tippy("#developer", {
+    content: "Developer",
+    animation: 'scale'
+    });
+    
+    tippy("#hypesquad", {
+    content: "HypeSquad Balance",
+    animation: 'scale'
+    });
+    
+    tippy("#nitro", {
+    content: "Assinante Nitro",
+    animation: 'scale'
+    });
+    
+    tippy("#nitroboost", {
+    content: "Assinante Boost",
+    animation: 'scale'
+    });
+      
+    tippy("#verified", {
+    animation: 'scale',
+    content: "Verificado"
+    });
+    
+    tippy("#oldname", {
+    animation: 'scale',
+    content: "Originally known as Try"
+    });
+    // tippy("#oldname2", {
+    // animation: 'scale',
+    // content: "Originally known as Schizosculptures#9333"
+    // });
